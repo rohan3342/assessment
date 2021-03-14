18fcdf3362d723449d4bd2bdd8cfe8b04d8b7fd1
Rohan Kumar Singh <rosingh3342@gmail.com>
2021-03-15 01:07:16 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Hello </Text>
+      </View>
+    );
+  }
+}
+
+export default App;
